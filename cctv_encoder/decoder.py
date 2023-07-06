27a0dc37bbeb5e0b531a9782d75b3e1240dc1912
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2023-07-06 05:20:50 +0000

    Adding composition options

--- cctv_encoder/decoder.py
+++ cctv_encoder/decoder.py
@@ -56,7 +56,7 @@ class Decoder:
         meta_data = encoded_data[keys[2]]
         return csr_m, background, meta_data
 
-    def decompress(self, csr_m, background):
+    def decompress(self, csr_m, background, compositon):
         """
         The decompress function takes a compressed matrix and the background image,
         and returns a aproximation for the original matrix.
@@ -66,11 +66,20 @@ class Decoder:
         :param self: Represent the instance of the class
         :param csr_m: Store the compressed sparse row matrix
         :param background: the background image
+        :param composition: Use the 'background', 'foreground' or 'both' for a composition
         :return: The aproximation for the original matrix A 
         """
-        S = csr_matrix((csr_m[0], csr_m[1], csr_m[2])).todense()
-        A_hat = (np.ones(S.shape).T * background).T
-        A_hat += S
+        if compositon == 'both':
+            S = csr_matrix((csr_m[0], csr_m[1], csr_m[2])).todense()
+            A_hat = (np.ones(S.shape).T * background).T
+            A_hat += S
+        elif compositon == 'background':
+            A_hat = (np.ones(S.shape).T * background).T
+        elif compositon == 'foreground':
+            A_hat = csr_matrix((csr_m[0], csr_m[1], csr_m[2])).todense()
+        else:
+            raise ValueError(
+                "if defined, composition must be one of: 'both', 'foreground' or 'background'")
         return A_hat
 
     def matrix_to_video(self, A_hat, width, height, fps):
@@ -86,22 +95,25 @@ class Decoder:
         :return: A video clip
         """
         frames = []
+        print("detected FPS:", fps)
         for frame_idx in range(A_hat.shape[1]):
             frame = matrix_to_image(A_hat, height, width, frame=frame_idx)
             frame = gray_to_3gray(frame.astype(np.uint8))
             frames.append(frame)
         return mpEdit.ImageSequenceClip(frames, fps=fps)
 
-    def decode(self, encoded_file_path):
+    def decode(self, encoded_file_path, out_file_name='', composition='both'):
         """
         The decode function takes in an encoded file path and save a decoded video file
         on the working directory.
         The function first loads the encoded data from the given path, then decompresses it using 
         the decompress function.
         Finally, it converts this matrix into a video and writes it to disk.
+        Optionally, you can get only the foreground or background.
 
         :param self: Bind the method to an object
         :param encoded_file_path: The encoded file path
+        :param composition: Use of the 'foreground', 'background' or defaut 'both' for a composition
         :return: The path to the decoded video in disk
         """
         self.encoded_file_path = encoded_file_path
@@ -109,13 +121,15 @@ class Decoder:
         self.width = meta_data[0]
         self.height = meta_data[1]
         self.fps = meta_data[2]
-        A_hat = self.decompress(csr_m, background)
+        A_hat = self.decompress(csr_m, background, composition)
         video = self.matrix_to_video(A_hat, self.width, self.height, self.fps)
-
-        video_name = self.encoded_file_path.split(
-                sep="/")[-1].split(sep=".")[0]
-        os.makedirs(self.work_directory + "/", exist_ok=True)
-        output_file_path = self.work_directory + "/" + video_name + "_decoded.mp4"
-        video.write_videofile(output_file_path, codec="libx264")
+        video.set_fps(self.fps)
+        if out_file_name == '':
+            out_file_name = self.encoded_file_path.split(
+                sep="/")[-1].split(sep=".")[0] + "_decoded.mp4"
+        os.makedirs(self.work_directory, exist_ok=True)
+        output_file_path = self.work_directory + out_file_name
+        # Make shure to update moviepy and ffmpeg
+        video.write_videofile(output_file_path, fps=self.fps, codec="libx264")
 
         return output_file_path
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from cctv_encoder import Encoder, Decoder
 
 # Encode process
 
-WORKDIR = "/workspaces/CCTV-Encoder/test_Files"
+WORKDIR = "/workspaces/CCTV-Encoder/test_Files/BallBounce/"
 
 my_enc = Encoder(WORKDIR)
 
@@ -13,9 +13,12 @@ my_enc.set_precision(1e-2)
 my_enc.set_quality(1)
 my_enc.set_PCP_n_iter(7)
 
-my_enc.encode(video_path = "video.mp4")
+my_enc.encode(video_path = "BallBounce_30sec_(144p).mp4")
 
 # Decode process
 
 decoder = Decoder(WORKDIR)
-decoder.decode(encoded_file_path = WORKDIR + "/video.npz")
+
+decoder.decode(encoded_file_path = WORKDIR + "BallBounce_30sec_(144p).npz",
+               out_file_name = "BallBounce_foreground_decoded.mp4",
+               composition = "foreground")
